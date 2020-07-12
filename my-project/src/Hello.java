547a283f72a3b99982443bfc86744a62dc7ed0a6
Dorota Wrobel <wrobel.do@gmail.com>
2020-07-12 12:50:34 +0200

    Mój pierwszy program

---
+++ my-project/src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args) {
+System.out.println("Witaj w programie!");
+        System.out.println("Witaj w programie!");
+        System.out.print("Witaj w programie!");
+        System.out.print("Witaj w programie!");
+        String name = "Dorota";
+        String surname = "Wróbel";
+        System.out.println(name + "" + surname);
+        System.out.println(name + "" + surname);
+        System.out.println("Koniec");
+    }
+}
