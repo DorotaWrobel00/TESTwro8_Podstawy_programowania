bcedcf3ce2ecf8fffc9e8a6559efbe7fc73a8503
tomasz.lis <S@m8un6$>
2020-07-23 18:21:34 +0200

    zadanie 9 lista d1

---
+++ lesson2/src/pl/sda/company/Company.java
@@ -0,0 +1,40 @@
+package pl.sda.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Klasa reprezentująca firmę
+ * wykonana w ramach zadania 9 z listy D1
+ */
+public class Company {
+    private String name;
+    private List<Department> departments = new ArrayList<>();
+
+    public void createDepartment(String name){
+        Department department = new Department();
+        department.setName(name);
+        department.setEmployees(new ArrayList<>());
+        departments.add(department);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Department> getDepartments() {
+        return departments;
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "name='" + name + '\'' +
+                ", departments=" + departments +
+                '}';
+    }
+}
